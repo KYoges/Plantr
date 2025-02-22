390a30d5a5cb9129568c521c281ac30da9623b59
Faran Zafar <faran.zafar@realitymine.com>
2025-02-22 22:07:02 +0000

    update req file

--- llm_api/google_calender.py
+++ llm_api/google_calender.py
@@ -37,13 +37,8 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 
 def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
+
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
